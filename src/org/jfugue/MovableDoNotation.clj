b8b4352c32ee8a4f093607265d93b1ff172401fa
Michael Wu <mw10013@gmail.com>
2010-11-15 01:12:09 -0500

    Created

---
+++ src/org/jfugue/MovableDoNotation.clj
@@ -0,0 +1,95 @@
+; Copyright (c) Michael Wu.  All rights reserved.
+; This library is free software; you can redistribute it and/or
+; modify it under the terms of the GNU Lesser General Public
+; License as published by the Free Software Foundation; either
+; version 2.1 of the License, or any later version.
+;
+; This library is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; Lesser General Public License for more details.
+;
+; You should have received a copy of the GNU Lesser General Public
+; License along with this library; if not, write to the Free Software
+; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+(ns org.jfugue.MovableDoNotation
+  (:import (org.jfugue Pattern Note MusicStringParser Player IntervalNotation))
+  (:use clojure.contrib.command-line)
+  (:gen-class
+   :init init
+   :constructors {[String] []}
+   :state state
+   :methods [[getMusicStringWithMovableDo [] String]
+             [setMusicStringWithMovableDo [String] void]
+             [getPatternForRootNote [String] org.jfugue.Pattern]
+             [getPatternForRootNote [org.jfugue.Pattern] org.jfugue.Pattern]
+             [getPatternForRootNote [org.jfugue.Note] org.jfugue.Pattern]
+             [compile [] void]]))
+
+; TODO: octave
+
+(def ^{:private true} degree-vector [0 2 4 5 7 9 11])
+
+(defn -compile [this]
+  (compile 'org.jfugue.MovableDoNotation))
+
+(defn -init [music-string]
+  [[] (atom {:music-string music-string})])
+
+(defn -getMusicStringWithMovableDo [this]
+  (:music-string @(.state this)))
+
+(defn -setMusicStringWithMovableDo [this music-string]
+  (swap! (.state this) assoc :music-string music-string))
+
+(defn -getPatternForRootNote-String [this music-string]
+;  (println "getPatternForRootNote - string")
+  (.getPatternForRootNote this (Pattern. music-string)))
+
+(defn -getPatternForRootNote-Pattern [this pattern]
+;  (println "getPatternForRootNote - pattern")
+  (.getPatternForRootNote this (MusicStringParser/getNote pattern)))
+
+(defn format-degree [{:keys [degree root-note-value]}]
+  (let [degree-normalized (dec degree) degree-vector-count (count degree-vector)]
+    (str "[" (+  root-note-value
+                 (* 12 (quot degree-normalized degree-vector-count)) 
+                 (nth degree-vector (mod degree-normalized degree-vector-count)))  "]")))
+ 
+(defn parse-char [m c]
+;  (println "m:" m)
+;  (println "c:" c ":")
+  (condp = (:state m)
+      :degree (cond
+               (Character/isDigit c) (assoc m :degree (+ (* (:degree m) 10) (Character/digit c 10)))
+               (Character/isWhitespace c) (assoc m :state :whitespace
+                                                 :buf (conj (:buf m) (format-degree m) " ") :degree 0)
+               :else (assoc m :state :char
+                            :buf (conj (:buf m) (if (= (:degree m) 0) "" (format-degree m)) c) :degree 0))
+      :whitespace (cond
+                   (Character/isDigit c) (assoc m :state :degree :degree (+ (* (:degree m) 10) (Character/digit c 10)))
+                   (Character/isWhitespace c) m
+                   :else (assoc m :state :char :buf (conj (:buf m) c)))
+      :char (cond
+             (or (= c \+) (= c \_)) (assoc m :state :degree :buf (conj (:buf m) c))
+             (Character/isWhitespace c) (assoc m :state :whitespace :buf (conj (:buf m) c))
+             :else (assoc m :buf (conj (:buf m) c)))))
+ 
+(defn -getPatternForRootNote-Note [this note]
+;  (println "getPatternForRootNote - note3")
+  (let [m (reduce parse-char {:state :degree :root-note-value (.getValue note) :degree 0 :buf []}
+                  (str (:music-string @(.state this)) " "))]
+    (Pattern. (apply str (:buf m)))))
+
+(defn -main [& args]
+  (with-command-line args
+      "MovableDoNotation: use scale degrees"
+      [[note "The root note for the pattern" 60]
+       [play? p? "Play."] strings]
+      (let [note (Note. (Integer. note))
+            player (if play? (Player.) nil)]
+        (doseq [s strings]
+          (let [music-string (.. (org.jfugue.MovableDoNotation. s) (getPatternForRootNote note) (getMusicString))]
+            (println s ": " music-string)
+            (if play? (.play player music-string)))))))
---
+++ test/org/jfugue/test/MovableDoNotation.clj
@@ -0,0 +1,42 @@
+; Copyright (c) Michael Wu.  All rights reserved.
+; This library is free software; you can redistribute it and/or
+; modify it under the terms of the GNU Lesser General Public
+; License as published by the Free Software Foundation; either
+; version 2.1 of the License, or any later version.
+;
+; This library is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; Lesser General Public License for more details.
+;
+; You should have received a copy of the GNU Lesser General Public
+; License along with this library; if not, write to the Free Software
+; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+(ns org.jfugue.test.MovableDoNotation
+  (:use [org.jfugue.MovableDoNotation] :reload)
+  (:use [clojure.test])
+  (:import (org.jfugue Pattern Note MusicStringParser Player IntervalNotation)))
+
+(defn interval-music-string [s n]
+  (.. (IntervalNotation. s) (getPatternForRootNote n) (getMusicString)))
+
+(defn movable-do-music-string [s n]
+  (.. (org.jfugue.MovableDoNotation. s) (getPatternForRootNote n) (getMusicString)))
+
+(defn compare-music-strings [s1 s2]
+   (let [n (Note. 60)]
+    (is (= (movable-do-music-string s1 n) (interval-music-string s2 n)))))
+
+(deftest try-1
+  (let [n (Note. 60)]
+    (is (= (interval-music-string "<1> <5> <8>" n) "[60] [64] [67] "))
+    (is (= (movable-do-music-string "1 3 5" n) "[60] [64] [67] "))
+    (compare-music-strings "1 3 5" "<1> <5> <8>")
+    (compare-music-strings "1 2 3 4 5 6 7 8 9 10 11 12" "<1> <3> <5> <6> <8> <10> <12> <13> <15> <17> <18> <20>")
+    (compare-music-strings "1h" "<1>h")
+    (compare-music-strings "1h+2h+3q_4qw" "<1>h+<3>h+<5>q_<6>qw")
+    (compare-music-strings "T[Allegro] V0 I0 1q 3q V1 1q 3q" "T[Allegro] V0 I0 <1>q <5>q V1 <1>q <5>q")
+    (compare-music-strings "V0 1majw V1 I[Flute] 3q 1q" "V0 <1>majw V1 I[Flute] <5>q <1>q")
+    (compare-music-strings "T120 V0 I[Piano] 1q 3q V9 [Hand_Clap]q Rq" "T120 V0 I[Piano] <1>q <5>q V9 [Hand_Clap]q Rq")
+    ))
