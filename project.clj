1fcfc1430e97b84d358ee2addc01cc98cc9ae1e6
Michael Wu <mw10013@gmail.com>
2010-11-26 21:56:18 -0500

    quick osc e2e

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject jfugue-movabledo "0.0.2"
+(defproject jfugue-movabledo "0.0.3"
   :description "Movable do support for JFugue (http://www.jfugue.org)"
   :license {:name "GNU Lesser General Public License 2.1"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [jfugue "4.0.3"]]
+                 [jfugue "4.0.3"]
+                 [org.clojars.mw10013/javaosc "20060402.0.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :aot [org.jfugue.MovableDoNotation]
   :main org.jfugue.MovableDoNotation)
--- src/org/jfugue/MovableDoNotation.clj
+++ src/org/jfugue/MovableDoNotation.clj
@@ -14,7 +14,10 @@
 ; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 (ns org.jfugue.MovableDoNotation
-  (:import (org.jfugue Pattern Note MusicStringParser Player IntervalNotation))
+  (:import (org.jfugue Pattern Note MusicStringParser Player IntervalNotation MusicStringParser MidiRenderer)
+           (javax.sound.midi Sequencer Sequence Transmitter Receiver ShortMessage MidiSystem)
+           (java.net InetAddress)
+           (com.illposed.osc OSCPort OSCPortOut OSCPortIn OSCMessage OSCListener))
   (:use clojure.contrib.command-line)
   (:gen-class
    :init init
@@ -86,14 +89,46 @@
                   (str (:music-string @(.state this)) " "))]
     (Pattern. (apply str (:buf m)))))
 
+(defn output-short-message [m type]
+  (println type ": " (.getData1 m) " " (.getData2 m) " chan: " (.getChannel m)))
+
+(defn decode-short-message [m osc-out]
+;  (println (class osc-out))
+;  (println "decode-short-message1: " (class m) ": " (.getCommand m) " " (.getData1 m) " " (.getData2 m) " chan: " (.getChannel m))
+  (condp = (.getCommand m)
+      (ShortMessage/NOTE_ON) (do (.send osc-out (OSCMessage. "/isynth/chan1/note" (object-array [(.getData1 m) (.getData2 m)]))) (output-short-message m "noteon"))
+      (ShortMessage/NOTE_OFF) (do (.send osc-out (OSCMessage. "/isynth/chan1/note" (object-array [(.getData1 m) 0]))) (output-short-message m "noteoff"))
+      (ShortMessage/CONTROL_CHANGE) (output-short-message m "cc")
+      (println "unknown: " (.getCommand m) " " (.getData1 m) " " (.getData2 m) " chan: " (.getChannel m))))
+
+(defn osc-play [music-string player]
+  (with-open [osc-out (OSCPortOut. (java.net.InetAddress/getLocalHost) 5432)]
+    (let [pattern (Pattern. music-string)
+          sequence (.getSequence player pattern)
+          sequencer (MidiSystem/getSequencer false)]
+      (.open sequencer)
+      (.. sequencer (getTransmitter) (setReceiver (reify Receiver
+                                                         (send [this midi-message timestamp]
+                                                               (when (instance? ShortMessage midi-message) (decode-short-message midi-message osc-out)))
+                                                         )))
+      (doto sequencer (.setSequence sequence) (.start))
+      (while (.isRunning sequencer) (Thread/sleep 20))
+      (.close sequencer)
+      ))
+  )
+
+(alter-var-root #'*out* (constantly *out*)) 
+
 (defn -main [& args]
   (with-command-line args
-      "MovableDoNotation: use scale degrees: 1 3 5 instead of <1> <5> <8>3"
+      "MovableDoNotation: use scale degrees: 1 3 5 instead of <1> <5> <8>"
       [[note "The root note for the pattern" 60]
-       [play? p? "Play."] strings]
+       [play? p? "Play."]
+       [osc? o? "OSC"] strings]
       (let [note (Note. (Integer. note))
-            player (if play? (Player.) nil)]
+            player (if (or play? osc?) (Player.) nil)]
         (doseq [s strings]
           (let [music-string (.. (org.jfugue.MovableDoNotation. s) (getPatternForRootNote note) (getMusicString))]
             (println s ": " music-string)
-            (if play? (.play player music-string)))))))
+            (if play? (.play player music-string))
+            (if osc? (osc-play music-string player)))))))
